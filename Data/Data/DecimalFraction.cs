a77b83ecc0f33a41dbea847586a630defe8dca09
ASUS <ASUS@ASUS->
2017-06-12 13:05:01 +0500

    Создала класс для Десятичной дроби и для обыкновенной

---
+++ Data/Data/DecimalFraction.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    //класс для десятичной дроби
+    public class DecimalFraction : Fraction 
+    {
+        private float value;//сама десятичная дробь
+        private int countSigns; //кол-во знаков после запятой
+
+        public float Value
+        {
+            get
+            {
+                return value;
+            }
+        }
+
+        public DecimalFraction(float value)//если на вход подана дробь
+        {
+            this.value = value;
+
+            //считаем количество знаков после запятой
+            countSigns = Value.ToString().Substring(Value.ToString().IndexOf(",") + 1).Length;
+
+            //if (countSigns > 15)
+            //    throw new Exception("Слишком много знаков после запятой!");
+        }
+
+        public DecimalFraction(string data)//если на вход подана строка
+        {
+            if (IsValid(data))
+            {
+                data = data.Replace(".", ",");
+
+                value = float.Parse(data);
+            }
+        }
+
+        public SimpleFraction ConvertTo() //преобразование десятичной дроби в обыкновенную
+        {
+            int numerator = Convert.ToInt32(Value * Math.Pow(10, countSigns));
+            int denominator = Convert.ToInt32(Math.Pow(10, countSigns));
+
+            return new SimpleFraction(numerator, denominator);
+        }
+
+        public static bool IsValid(string data) //проверка является ли дробь десятичной
+        {
+            return data.Contains(".") || data.Contains(",");
+        }
+
+        public static DecimalFraction operator +(DecimalFraction fr1, DecimalFraction fr2)
+        {
+            DecimalFraction res = new DecimalFraction(fr1.Value + fr2.Value);
+
+            return res;
+        }
+
+        public static DecimalFraction operator -(DecimalFraction fr1, DecimalFraction fr2)
+        {
+            DecimalFraction res = new DecimalFraction(fr1.Value - fr2.Value);
+
+            return res;
+        }
+
+        public static DecimalFraction operator *(DecimalFraction fr1, DecimalFraction fr2)
+        {
+            DecimalFraction res = new DecimalFraction(fr1.Value * fr2.Value);
+
+            return res;
+        }
+
+        public static DecimalFraction operator /(DecimalFraction fr1, DecimalFraction fr2)
+        {
+            DecimalFraction res = new DecimalFraction(fr1.Value / fr2.Value);
+
+            return res;
+        }
+
+    }
+}
---
+++ Data/Data/SimpleFraction.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    //класс для обыкновенной дроби
+
+    public class SimpleFraction : Fraction
+    {
+        private int numerator;//числитель
+        private int denominator;//знаменатель
+
+        public int Nominator
+        {
+            get
+            {
+                return numerator;
+            }
+        }
+
+        public int Denominator
+        {
+            get
+            {
+                return denominator;
+            }
+        }
+
+        public SimpleFraction(int nominator, int denominator)
+        {
+            this.numerator = nominator;
+            this.denominator = denominator;
+        }
+
+        public static bool IsValid(string data)//проверка является ли дробь обыкновенной
+        {
+            return data.Contains("/");
+        }
+
+        public SimpleFraction(string data)
+        {
+            if (IsValid(data))
+            {
+                numerator = int.Parse(data.Substring(0, data.IndexOf("/")));
+                denominator = int.Parse(data.Substring(data.IndexOf("/") + 1, data.Length - data.IndexOf("/") - 1));
+            }
+            else
+            {
+                throw new Exception("Не удалось преобразовать в обыкновенную дробь!");
+            }
+        }
+
+        public DecimalFraction ConvertTo()//преобразование обыкновенной в десятичную
+        {
+            float val = (float)Nominator / Denominator;
+
+            DecimalFraction decimalFraction = new DecimalFraction(val);
+
+            return decimalFraction;
+        }
+
+        public static SimpleFraction operator +(SimpleFraction fr1, SimpleFraction fr2)
+        {
+            int numerator = fr1.Nominator * fr2.Denominator + fr2.Nominator * fr1.Denominator;
+            int denominator = fr1.Denominator * fr2.Denominator;
+
+            SimpleFraction res = new SimpleFraction(numerator, denominator);
+
+            return res;
+        }
+
+        public static SimpleFraction operator -(SimpleFraction fr1, SimpleFraction fr2)
+        {
+            int numerator = fr1.Nominator * fr2.Denominator - fr2.Nominator * fr1.Denominator;
+            int denominator = fr1.Denominator * fr2.Denominator;
+
+            SimpleFraction res = new SimpleFraction(numerator, denominator);
+
+            return res;
+        }
+
+        public static SimpleFraction operator *(SimpleFraction fr1, SimpleFraction fr2)
+        {
+            int numerator = fr1.Nominator * fr2.Nominator;
+            int denominator = fr1.Denominator * fr2.Denominator;
+
+            SimpleFraction res = new SimpleFraction(numerator, denominator);
+
+            return res;
+        }
+
+        public static SimpleFraction operator /(SimpleFraction fr1, SimpleFraction fr2)
+        {
+            int numerator = fr1.Nominator * fr2.Denominator;
+            int denominator = fr1.Denominator * fr2.Nominator;
+
+            SimpleFraction res = new SimpleFraction(numerator, denominator);
+
+            return res;
+        }
+    }
+}
